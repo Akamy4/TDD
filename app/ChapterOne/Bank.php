eecb5fae6cc55c1d069f445f5aa15124c01be735
aznabakiev <aznabakiev@spark.kx>
2024-05-29 01:06:36 +0500

    main 3

--- app/ChapterOne/Bank.php
+++ app/ChapterOne/Bank.php
@@ -19,19 +19,16 @@ final class Bank
         return $source->reduce($this, $to);
     }
 
-    // Метод для установки значения в коллекцию
     public function addRate(string $from, string $to, int $rate): void
     {
         $this->rates->put($from . '_' .$to, $rate);
     }
 
-    // Метод для получения значения из коллекции
     public function getRate(string $from, string $to): int
     {
         return (int)$this->rates->get($from . '_' .$to);
     }
 
-    // Метод для удаления значения из коллекции
     public function removeRate($key): void
     {
         $this->rates->forget($key);
--- app/ChapterOne/Pair.php
+++ app/ChapterOne/Pair.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\ChapterOne;
 
-use PhpParser\Node\Expr\Cast\Object_;
-
 final class Pair
 {
     public function __construct(
